48e134425670622d46d457ff15e02a55494246ee
FlavGg <103735029+FlavGg@users.noreply.github.com>
2024-11-22 20:26:37 +0100

    Fixed Redwood Slabs (fully working now with custom BlockStateDefinition) - FlavGg

--- src/main/java/net/foxbatstudios/overworld_revived/block/ModBlocks.java
+++ src/main/java/net/foxbatstudios/overworld_revived/block/ModBlocks.java
@@ -6,6 +6,7 @@ import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.SlabBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -28,7 +29,7 @@ public class ModBlocks {
     public static final RegistryObject<Block> REDWOOD_WOOD = registerBlock("redwood_wood",
             () -> new Block(BlockBehaviour.Properties.copy(Blocks.OAK_WOOD)));
     public static final RegistryObject<Block> REDWOOD_SLABS = registerBlock("redwood_slabs",
-            () -> new Block(BlockBehaviour.Properties.copy(Blocks.OAK_SLAB)));
+            () -> new SlabBlock(BlockBehaviour.Properties.copy(Blocks.OAK_SLAB)));
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
